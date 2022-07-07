980a21caf89e5a75ca88965e63721ca974b80c5f
Vincent Bayode <ebayode00@gmail.com>
2022-07-07 08:28:09 -0400

    current changes

--- script.js
+++ script.js
@@ -1,19 +1,88 @@
 
 $(document).ready(function () {
-    $("p").hover(function () {
-            // over
-            $("p").css("color", "#ff0000");
-        }, function () {
-            // 
-            $("p").css("color", "#000000");
-        }
+    $("h2").hover(function () {
+        // over
+        $("h2").css("color", "#9615DB");
+    }, function () {
+        // 
+        $("h2").css("color", "#7D7F80");
+    }
     );
-var windowWidth = $(this).width();
-$(window).resize(function () { 
-    windowWidth = $(this).width();
-    console.log (windowWidth);   
-    if (windowWidth >= 715){
-        $("p").css("font-size", "16px");
+    var windowWidth = $(this).width();
+    $(window).resize(function () {
+        windowWidth = $(this).width();
+        console.log(windowWidth);
+        if (windowWidth >= 715) {
+            $("h2").css("font-size", "16px");
+        }
+    });
+
+});
+
+$("h3").hover(function () {
+        // over
+        $("h3").css("color", "#9615DB");
+    }, function () {
+        // 
+        $("h3").css("color", "#7D7F80");
+    }
+    );
+    var windowWidth = $(this).width();
+    $(window).resize(function () {
+        windowWidth = $(this).width();
+        console.log(windowWidth);
+        if (windowWidth >= 715) {
+            $("h3").css("font-size", "16px");
+        }
+    });
+
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
-});
-});
+
+$("hobbies").hover(function () {
+        // over
+        $("hobbies").css("color", "#9615DB");
+    }, function () {
+        // 
+        $("hobbies").css("color", "#000000");
+    }
+    );
+    var windowWidth = $(this).width();
+    $(window).resize(function () {
+        windowWidth = $(this).width();
+        console.log(windowWidth);
+        if (windowWidth >= 715) {
+            $("hobbies").css("font-size", "16px");
+        }
+    });
+
+
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    h = checkTime(h);
+    var time = h + ":" + m + ":" + s;
+    $(".time").text(time);
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    } 
+    return i;
+}
+
